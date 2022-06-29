0c0ba4b2c39d163ce27fe688f7ee96f6c510f0f2
thfilipe <thomasfilipe@yahoo.com>
2022-06-29 19:52:05 +0100

    Completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,6 @@
-const repeatString = function() {
+const repeatString = function (word, num) {
 
+    return (num < 0) ? 'ERROR' : word.repeat(num)
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, num) {
+    for (let arr of array) {
 
+    }
+    return array
 };
 
 // Do not edit below this line
