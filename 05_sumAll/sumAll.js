d4bb5b71c9438e2872385fac29961327194c3c3b
thfilipe <thomasfilipe@yahoo.com>
2022-07-01 18:04:41 +0100

    Complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    let total = 0
 
+    if (!Number.isInteger(start) || !Number.isInteger(end)) return "ERROR"
+    if (start < 0 || end < 0) return 'ERROR';
+
+
+    if (start < end) {
+        for (let i = start; i <= end; i++) {
+            total += i;
+        }
+    }
+    for (let i = end; i <= start; i++) {
+        total += i;
+    }
+
+
+
+
+    return total
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
+const ftoc = function (farenheit) {
+  let celsius = (farenheit - 32) * (5 / 9)
+  return Math.round(celsius * 10) / 10
 
 };
 
-const ctof = function() {
-
+const ctof = function (celsius) {
+  let farenheit = celsius * (9 / 5) + 32
+  return Math.round(farenheit * 10) / 10
 };
 
 // Do not edit below this line
